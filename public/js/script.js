51959f795d42ba735c27bff7c53388335766e2f2
Stephen <stephen.j.tong@gmail.com>
2013-04-09 08:04:46 +0800

    add close to quick add

--- public/js/script.js
+++ public/js/script.js
@@ -3,10 +3,14 @@ $(function(){
         el: $("#reader"),
         events: {
                     "click #lhn-add-subscription"       : "quickaddurl",
+                    "click #quick-add-close"            : "quickAddClose",
                 },
     quickaddurl: function() {
                      $("#quick-add-bubble-holder").attr('class', '');
                   },
+    quickAddClose: function() {
+                       $("#quick-add-bubble-holder").attr('class', 'hidden');
+                   }
     });
 
     window.App = new AppView;
