22f552a64c6c426f11aa8779d9b22db5e15c4a13
Stephen <stephen.j.tong@gmail.com>
2013-04-24 22:55:00 +0800

    register form

--- public/js/script.js
+++ public/js/script.js
@@ -1,10 +1,12 @@
 $(function(){
     window.AppView = Backbone.View.extend({
-        el: $("#reader"),
+        el: $("body"),
         events: {
                     "click #lhn-add-subscription"       : "quickaddurl",
                     "click #quick-add-close"            : "quickAddClose",
                     "click #quick-add-btn"              : "addFeed",
+                    "click #register_link"              : "showRegisterForm",
+                    "click #register-form-close"       : "closeRegisterForm",
                 },
     quickaddurl: function() {
                      $("#quick-add-bubble-holder").attr('class', '');
@@ -12,6 +14,13 @@ $(function(){
     quickAddClose: function() {
                        $("#quick-add-bubble-holder").attr('class', 'hidden');
                    },
+    showRegisterForm: function(){
+                       $("#register").attr('class', '');
+                      },
+    closeRegisterForm: function(){
+                           $("#register").attr('class', 'hidden');
+                       },
+
     addFeed: function(){
                  $("#loading-area-container").removeClass('hidden');
                  $("#quick-add-bubble-holder").attr('class', 'hidden');
