cf4807c5781a1bc609ff2fecaed0f692141a37f4
Stephen <stephen.j.tong@gmail.com>
2013-04-27 22:19:01 +0800

    add user model

---
+++ models/user.rb
@@ -0,0 +1,10 @@
+require 'digest/sha1'
+
+class Feed < ActiveRecord::Base
+    before_save :generate_url_hash
+
+    def generate_url_hash
+        self.hashed_url = Digest::SHA1.hexdigest self.url
+    end
+
+end
